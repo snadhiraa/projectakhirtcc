5aee1f7287adb00f063cb4314e01840422d5f422
snadhiraa2@gm <shofia1912@gmail.com>
2024-06-07 17:38:25 +0700

    frontend

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+    console.log(`Frontend service is running on port ${PORT}`);
+});
